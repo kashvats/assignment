e473da3d270d663ca0ef963c8adb0c2d5b1f0085
kashvats29 <vatsakash168@gmail.com>
2023-05-03 01:24:40 +0530

    static

--- assignment/urls.py
+++ assignment/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 # from ecom.products import urls
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('products.urls')),
 ]
+
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
